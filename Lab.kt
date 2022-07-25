1af393fc16f7d3418a4b56007b1ac5268bef6cbf
AstridGlauser1 <109930941+AstridGlauser1@users.noreply.github.com>
2022-07-25 05:11:23 +0200

    Update Lab.kt

--- Lab.kt
+++ Lab.kt
@@ -9,88 +9,22 @@ data class ItemData(
     var info: String? = null
 )
 // -----------------------
-
 fun main() {
     val result = processList(listOf(25, "Hola", null, false))
-    println(result)
-    
-}
-
+    println(result) }
 fun processList(inputList: List<Any?>?): List<ItemData>? {
    if (inputList == null) { 
         return null
-    } else {
+   } else {
          var result = ArrayList<ItemData>()
-       
-        for ((indice, item) in inputList.withIndex()) {
-            when (item) {
-              is  String -> result.add(ItemData(
-   	originalPos = indice,
-   	originalValue = item,
-  	type = "cadena",
-  	info = ParaString(item)
-   )
-              )
-              is Boolean -> result.add(ItemData(
-              	originalPos = indice,
-   				originalValue = item,
-  				type = "Booleano",
-  				info = ParaBooleano(item)
-              
-              ))
-              
-              is Int -> result.add(ItemData(
-              
-                originalPos = indice,
-   				originalValue = item,
-  				type = "Entero",
-  				info = ParaEnteros(item)
-              ))
-              
-           else-> 
-               if(item!=null){
-                 result.add(ItemData(
-                originalPos = indice,
-   				originalValue = item,
-  				type = "null",
-  				info = null  
-                   )
-                 )
-               }
-           
-                
-            }
-        
-}    
+        for ((indice, item) in inputList.withIndex()) { 
+ when (item) {
+ is  String -> result.add(ItemData( indice,item,"cadena","L" +item.length.toString()))
+ is Boolean -> result.add(ItemData(indice, item, "Booleano", if(item){"Verdadero"}else{"Falso"}))
+ is Int -> result.add(ItemData( indice,item, "Entero", if(item%10==0){"M10"}else if(item%5==0){"M5"}else if(item%2==0) {"M2"}else{null}))
+ else-> if(item!=null){result.add(ItemData( indice, item, "null",null))}
+           }
+        }    
       return result   
-    }
-
-}
-
-fun ParaString(cadena:String):String?{
-    val numero:Int=(cadena.length)
-    return "L"+numero.toString()
-}
-   
-fun ParaEnteros(num:Int):String?{
-    if(num%10==0){
-        return "M10"
-        
-    }else if(num%5==0){
-        return "M5"
-    }
-    else if(num%2==0){
-        return "M2"
-    }
-    else{
-        return null
-    }
-       
-    }
-fun ParaBooleano(bo:Boolean):String?{
-    if(bo){
-        return "Verdadero"
-    }else{
-        return "Falso"
     }
 }
